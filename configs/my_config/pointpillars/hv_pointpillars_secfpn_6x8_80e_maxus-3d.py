aa99d6291574fc2d41d889604b7303b84efc771f
Sean Zhang <sean_zhang777@163.com>
2021-04-10 14:22:59 +0800

    add fpn,freeanchor,regnet config for maxus dataset

---
+++ configs/my_config/pointpillars/hv_pointpillars_secfpn_6x8_80e_maxus-3d.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_secfpn_maxus.py',
+    '../_base_/datasets/maxus-3d.py',
+    '../_base_/schedules/cyclic_80e.py', '../_base_/default_runtime.py'
+]
