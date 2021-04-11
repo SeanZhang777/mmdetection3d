fca798abed7a5553250774f4273e4665e53053b9
SeanZhang777 <46335248+SeanZhang777@users.noreply.github.com>
2021-04-11 22:34:11 +0800

    fix free_anchor config

--- configs/my_config/pointpillars/hv_pointpillars_fpn_sbn-all_free-anchor_6x8_80e_maxus-3d.py
+++ configs/my_config/pointpillars/hv_pointpillars_fpn_sbn-all_free-anchor_6x8_80e_maxus-3d.py
@@ -27,6 +27,22 @@ model = dict(
                 [0.903, 1.950, 1.637],  # Cyclist
             ],
             rotations=[0, 1.57],
-            reshape_out=True))),
-        assigner_per_size=False))
+            reshape_out=True),
+        assigner_per_size=False,
+        diff_rad_by_sin=True,
+        dir_offset=0.7854,  # pi/4
+        dir_limit_offset=0,
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder', code_size=7),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=0.8),
+        loss_dir=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
+    # model training and testing settings
+    train_cfg=dict(
+        pts=dict(code_weight=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])))
         
