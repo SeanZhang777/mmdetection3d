ed62cc14c96990a64dca955af18324a100514314
Sean Zhang <sean_zhang777@163.com>
2021-06-14 16:45:00 +0800

    add roboM1 config

---
+++ configs/my_config/pointpillars/hv_pointpillars_myfpn_sbn-all_free-anchor_6x8_80e_maxus-roboM1-3d.py
@@ -0,0 +1,48 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_myfpn_maxus-roboM1.py',
+    '../_base_/datasets/maxus-roboM1-3d.py', '../_base_/schedules/cyclic_80e.py',
+    '../_base_/default_runtime.py'
+]
+
+model = dict(
+    pts_bbox_head=dict(
+        _delete_=True,
+        type='FreeAnchor3DHead',
+        num_classes=5,
+        in_channels=256,
+        feat_channels=256,
+        use_direction_classifier=True,
+        pre_anchor_topk=25,
+        bbox_thr=0.5,
+        gamma=2.0,
+        alpha=0.5,
+        anchor_generator=dict(
+            type='AlignedAnchor3DRangeGenerator',
+            ranges=[[0.0, -50.0, 0.0, 100.0, 50.0, 0.0]],
+            scales=[1, 2, 4],
+            sizes=[
+                [0.8660, 2.5981, 1.],
+                [0.5774, 1.7321, 1.],
+                [1., 1., 1.],
+                [0.4, 0.4, 1.],
+            ],
+            rotations=[0, 1.57],
+            reshape_out=True),
+        assigner_per_size=False,
+        diff_rad_by_sin=True,
+        dir_offset=0.7854,  # pi/4
+        dir_limit_offset=0,
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder', code_size=7),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=0.8),
+        loss_dir=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
+    # model training and testing settings
+    train_cfg=dict(
+        pts=dict(code_weight=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])))
+        
